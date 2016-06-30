6b62b8e37fccc73ebedf4506b58325fa75f01e56
hawk_wei <hawk_wei@htc.com>
2016-06-30 15:40:21 +0800

    1. Detect the HOME_KEY for the widget power saving.

--- app/src/main/java/com/hawk/widget/MainActivity.java
+++ app/src/main/java/com/hawk/widget/MainActivity.java
@@ -1,11 +1,17 @@
 
 package com.hawk.widget;
 
+import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.hawk.widget.utils.HomeWatcher;
 import com.hawk.widget.utils.OnHomePressedListener;
@@ -13,20 +19,35 @@ import com.hawk.widget.utils.SMLog;
 import com.hawk.widget.utils.UStats;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mMyBtn;
+    TextView mMyTv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMyBtn  = (Button) findViewById(R.id.Button1);
+        mMyTv2  = (TextView) findViewById(R.id.textView2);
+        mMyBtn.setOnClickListener(mMyToggleBtnListener);
+
         this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
         //Check if permission enabled
         if (!UStats.isUsageStatsON(this)){
+            mMyTv2.setText("please ENABLE this APP.");
+        }
+        else{
+            mMyTv2.setText("don't change this setting.");
+        }
+    }
+
+    private View.OnClickListener mMyToggleBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Please ENABLE it!", Toast.LENGTH_SHORT).show();
             // Settings\Security\Apps with usage access
             Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
             startActivity(intent);
         }
-    }
-
-
+    };
 
     @Override
     protected void onResume() {
